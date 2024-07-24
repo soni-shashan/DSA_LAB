c249eac5bdd90e9752c84eb0d458a083c859381f
soni-shashan <lumbhanishashan1510@gmail.com>
2024-07-24 22:03:24 +0530

    Update PRAC_RADIX.java

--- DAY 7/PRAC_RADIX.java
+++ DAY 7/PRAC_RADIX.java
@@ -8,10 +8,18 @@ public class PRAC_RADIX {
         ans=radixSort.dessending(arr);
         System.out.println("Array In descending order : "+ Arrays.toString(ans));
     }
+    //assending
     //4,5,76,7,2,-54,6,7,-85,-65,-4,-23,45,356
     //arr1 => 4,5,76,7,2,6,7,45,356
     //arr2 => -54,-85,-65,-4,-23
     //ans1 => -85,-65,-54,-23,-4 descending use -(arr[i]/10)%10 for indexing
     //ans2 => 2,4,5,7,7,45,76,356 assending
     //ans= ans1 merge with ans2
+    //dessending
+    //4,5,76,7,2,-54,6,7,-85,-65,-4,-23,45,356
+    //arr1 => 4,5,76,7,2,6,7,45,356
+    //arr2 => -54,-85,-65,-4,-23
+    //ans1 => -4,-23,-54,-65,-85 asscending use -(arr[i]/10)%10 for indexing
+    //ans2 => 356,76,45,7,7,5,4,2 desending
+    //ans= ans2 merge with ans1
 }
