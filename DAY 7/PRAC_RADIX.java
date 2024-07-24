07202426e982aafb3dc55334bb33dde24f3228a6
soni-shashan <lumbhanishashan1510@gmail.com>
2024-07-24 21:35:58 +0530

    radixSort With Negative

--- DAY 7/PRAC_RADIX.java
+++ DAY 7/PRAC_RADIX.java
@@ -2,10 +2,16 @@ import java.util.Arrays;
 
 public class PRAC_RADIX {
     public static void main(String[] args) {
-        int[] arr={4,57,156,7,25,45};
+        int[] arr={4,5,7,2,-54,6,7,-85,-65,-4,-23,76,45,356};
         int[] ans=radixSort.assending(arr);
         System.out.println("Array In ascending order : "+ Arrays.toString(ans));
-        ans=radixSort.dessending(arr);
-        System.out.println("Array In descending order : "+ Arrays.toString(ans));
+        // ans=radixSort.dessending(arr);
+        // System.out.println("Array In descending order : "+ Arrays.toString(ans));
     }
+    //4,5,76,7,2,-54,6,7,-85,-65,-4,-23,45,356
+    //arr1 => 4,5,76,7,2,6,7,45,356
+    //arr2 => -54,-85,-65,-4,-23
+    //ans1 => -85,-65,-54,-23,-4 descending use -(arr[i]/10)%10 for indexing
+    //ans2 => 2,4,5,7,7,45,76,356 assending
+    //ans= ans1 merge with ans2
 }
--- DAY 7/radixSort.java
+++ DAY 7/radixSort.java
@@ -1,17 +1,60 @@
 import java.util.Arrays;
 
 public class radixSort {
+    public static int p=0;
+    public static int n=0;
     public static int[] assending(int[] arr){
         int max=arr[0];
-        int len=0;
-        for(int x:arr){
+        int min=arr[0];
+        int len=0,len_x=0;
+        for(int i=0,x=arr[0];i<arr.length;i++){
+            x=arr[i];
             if(x>max){
                 max=x;
             }
+            if(x<min){
+                min=x;
+            }
         }
-        for(;max!=0;len++,max/=10);
-        for(int pos=1;len>0;pos*=10,len--){
-            arr=assradixCount(arr, pos);
+        for(;max!=0;len++,len_x++,max/=10);
+        if(min>=0){
+            for(int pos=1;len>0;pos*=10,len--){
+                arr=assradixCount(arr, pos);
+            }
+        }else{
+            for(int i=0;i<arr.length;i++){
+                if(arr[i]>=0){
+                    p++;
+                }else{
+                    n++;
+                }
+            }
+            int[] arr1=new int[p];
+            int[] arr2=new int[n];
+            int[] ans=new int[arr.length];
+            for(int i=0,y=0,z=0;i<arr.length;i++){
+                if(arr[i]>=0){
+                    arr1[y]=arr[i];
+                    y++;
+                }else{
+                    arr2[z]=arr[i];
+                    z++;
+                }
+            }
+            for(int pos=1;len_x>0;pos*=10,len_x--){
+                arr1=assradixCount(arr1, pos);
+            }
+            for(int pos=1;len>0;pos*=10,len--){
+                arr2=negderadixCount(arr2, pos);
+            }
+            for(int i=0;i<arr.length;i++){
+                if(i<arr2.length){
+                    ans[i]=arr2[i];
+                }else{
+                    ans[i]=arr1[i-arr2.length];
+                }
+            }
+            return ans;
         }
         return arr;
     }
@@ -56,6 +99,21 @@ public class radixSort {
         }
         for(int i=0;i<arr.length;i++){
             ans[arr.length-(--temp[(arr[i]/pos)%10])-1]=arr[i];
+        }
+        return ans;
+    }
+    public static int[] negderadixCount(int[] arr,int pos){
+        int[] ans=new int[arr.length];
+        int[] temp=new int[10];
+        Arrays.fill(temp, 0);
+        for(int i=0;i<arr.length;i++){
+            temp[-(arr[i]/pos)%10]++;
+        }
+        for(int i=1;i<temp.length;i++){
+            temp[i]+=temp[i-1];
+        }
+        for(int i=0;i<arr.length;i++){
+            ans[arr.length-(--temp[-(arr[i]/pos)%10])-1]=arr[i];
         }
         return ans;
     }
